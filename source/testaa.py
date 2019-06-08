53ec1d51ddd945ab66720c53978855271d1b197d
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-06-08 00:06:26 -0700

    First Commit

---
+++ source/testaa.py
@@ -0,0 +1,18 @@
+from clioten import IDevice, Device
+from time import sleep
+
+class LiveDevice (IDevice):
+    def oncomm(self,data,objId):
+        print("received: "+data+" "+objId)
+        if Device.mode == 1:
+            Device.mode = 0
+            data =  "Gonderilen veri"
+            Device.Updateresult(Device.who, "result data" ,data)
+            print("result sent: " + data )
+            sleep(0.1)
+
+if __name__ == '__main__':
+    device = LiveDevice
+    deviceId =  "0U7kUy3lCG"
+    Device.createDevice(device,"www.clioten.com" , "denemdemQe5SiKDVFk", "clfpIaRtAuhJDSGCeb4b6R" ,deviceId)
+
